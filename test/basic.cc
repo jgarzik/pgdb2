fc3ffec7734c01c8ccfc9cbc2eb66af410035ea7
Jeff Garzik <jeff@bloq.com>
2015-10-25 00:20:03 -0400

    [test] basic: test for constructor failure upon non-existent db

--- test/basic.cc
+++ test/basic.cc
@@ -1,14 +1,39 @@
 
 #include <pgdb2.h>
+#include <stdexcept>
+#include <assert.h>
 
 int main (int argc, char *argv[])
 {
 	pagedb::Options opts;
 	opts.f_read = true;
+	opts.f_write = false;
+	opts.f_create = false;
+
+	// TEST: fail, b/c foo.db does not exist
+	bool saw_err = false;
+	try {
+		pagedb::DB db("foo.db", opts);
+	}
+	catch (const std::runtime_error& error) {
+		saw_err = true;
+	}
+	catch (...) {
+		assert(0);
+	}
+	assert(saw_err == true);
+
+	// TEST: open/close foo.db
+	opts.f_read = true;
 	opts.f_write = true;
 	opts.f_create = true;
 
-	pagedb::DB db("foo.db", opts);
+	try {
+		pagedb::DB db("foo.db", opts);
+	}
+	catch (...) {
+		assert(0);
+	}
 
 	return 0;
 }
